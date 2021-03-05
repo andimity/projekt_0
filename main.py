ff42c1a33613a35e5dcbf481406b9dfc86806f00
andreashunziker@gmx.net <andreashunziker@gmx.net>
2021-03-05 11:58:35 +0100

    data versuch

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# This is a sample Python script.
+from sklearn import linear_model
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+dataset = np.loadtxt('Data/train.csv',delimiter=",",comments="#")
+print(dataset[1:2,:])
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
